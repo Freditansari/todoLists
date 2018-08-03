2fe4872b496c74eb6384dbba6a1fd4cc8e219a6d
Freditansari <Freditansari@users.noreply.github.com>
2018-08-03 10:26:57 +0700

    pre-final project
    pre-final project. oauth2 complete. all we have to do research on how to do full database implementation.
    
    forms and event bindings are clear.

--- frontEndApplications/src/app/app.module.ts
+++ frontEndApplications/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http'
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from '@angular/router';
+import { NotFoundComponent } from './Components/not-found/not-found.component';
+import { ContactComponent } from './Components/contact/contact.component';
+import { HomeComponent } from './Components/home/home.component';
+
+const appRoutes:Routes =[
+  {path:"home", component:HomeComponent},
+  {path:"contact", component:ContactComponent},
+  {path:"*", component:NotFoundComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule, 
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
