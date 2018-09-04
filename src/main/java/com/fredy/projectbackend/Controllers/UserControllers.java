36d0f93e87dffa1e77d0b04da46d386608ef6ab7
Freditansari <Freditansari@users.noreply.github.com>
2018-09-04 13:57:19 +0700

    added check logged in user

--- src/main/java/com/fredy/projectbackend/Controllers/UserControllers.java
+++ src/main/java/com/fredy/projectbackend/Controllers/UserControllers.java
@@ -1,6 +1,8 @@
 package com.fredy.projectbackend.Controllers;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +15,9 @@ public class UserControllers {
 
 
     @RequestMapping(value = "/api/getUserName", method = RequestMethod.POST , produces = "application/json")
-    public String getUserName(Principal principal){
+    public ResponseEntity<String> getUserName(Principal principal){
 
 
-        return principal.getName();
+        return new ResponseEntity<String>(principal.getName(),HttpStatus.OK);
     }
 }
